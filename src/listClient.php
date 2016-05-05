7c22e275e4d96ae60f8763bcf338fe2970cd555f
MaximeBi <maxime.bigonne@gmail.com>
2016-05-05 15:41:56 +0200

    add link to the id

--- src/listClient.php
+++ src/listClient.php
@@ -21,16 +21,22 @@
           <td><strong>ID</strong></td><td><strong>PRENOM</strong></td><td><strong>NOM</strong></td><td><strong>NUMERO</strong></td><td><strong>MAIL</strong></td>
         </tr>
        <?php
-        while ($row = pg_fetch_row($result))
+        while ($row = pg_fetch_row($result))//Parcourir toutes les lignes -> Tant que $row != NULL
         {
 	         echo '<tr>';
-	          $count = count($row);
+	          $count = count($row);//On compte combien de valeurs
 	           $y = 0;
             	while ($y < $count-1)
             	{
-            		$c_row = current($row);
-            		echo '<td>' . $c_row . '</td>';
-            		next($row);
+            		$c_row = current($row);//On recupère la première valeur
+
+                if($y == 0){
+                  echo'<td><a href="#">'.$c_row.'</a></td>';
+                }else{
+                  echo '<td>' . $c_row . '</td>';//On affiche la valeur
+                }
+
+            		next($row);//On passe à la suivante
             		$y = $y + 1;
             	}
             	echo '</tr>';
