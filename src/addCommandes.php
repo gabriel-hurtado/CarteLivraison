500d191c051617defbe53113ee8217a1399af1f5
Gabriel Hurtado <arellewen@gmail.com>
2016-05-09 22:42:24 +0200

    First implementation of orders

--- src/addCommandes.php
+++ src/addCommandes.php
@@ -1,1 +1,67 @@
-addCommandes.php
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
+</head>
+
+<body>
+
+
+<?php
+echo "<h2>Ajout d'une commande'</h2>";
+echo "<div align=center>";
+
+	$id_produit = $_POST['id_produit'];
+	$numero_cl= $_POST["numero_cl"];
+	$jour= $_POST["jour"];
+	$mois= $_POST["mois"];
+	$annee= $_POST["annee"];
+	$pattern = '/[][(){}<>\/+²"*%&=?`"\'^\!$_:;,]/';
+
+
+
+if (empty($jour) or empty($mois) or empty($annee)) {
+		$jour = '01';
+		$mois = '01';
+		$annee = '1970';
+}
+
+	$date=$annee."-".$mois."-".$jour;
+	
+	if($date != '1970-01-01'){
+		/* Date du jour */
+		date_default_timezone_set('Europe/Paris');
+		$today = date("Ymd");
+		// on formate les dates selon le format Ymd
+		$today = new DateTime( $today );
+		$today = $today->format("Ymd");
+		$date = new DateTime( $date );
+		$date = $date->format("Ymd");
+		/* On vérifie que la date est valide*/
+		if (!checkdate($mois, $jour, $annee)){
+				echo "Veuillez rentrer une date valide <br>";
+				echo "<input type='button' value='Retour' onClick='history.go(-1)'>";
+			}
+		else if( $today > $date ) {
+			echo "Le délai de livraison ne peut etre dans le passé...<br>";
+		  	echo "<input type='button' value='Retour' onClick='history.go(-1)'>";
+		}
+	}
+
+if ($date == '1970-01-01' || $today <= $date){
+	/*Si aucune erreur, alors on peut enregistrer dans la BDD*/
+	include "connect.php";
+
+	$vConnect = fConnect();
+    //Ajout de la Marchandise
+   // $vSql3 = "INSERT INTO proCommande(id, livree, date_livraison, date_commande, enquete_satisfaction_envoyee, reponse_enquete_satisfaction, numero_client) VALUES (DEFAULT, 'FALSE', '$date', '$today', 'FALSE', NULL, '$numero_cl')";
+		$vResult3 = pg_query($vConnect, $vSql3);
+		echo "la marchandise a bien été ajoutée, il faudrait faire pareil avec les produits !";
+		pg_close($vConnect);
+}
+
+echo "</div>";
+?>
+
+</body>
+</html>
---
+++ src/commandesForm.php
@@ -0,0 +1,108 @@
+<html>
+<head>
+<meta charset="UTF-8" />
+</head>
+
+<body>
+	<form method="POST" action="addCommandes.php">
+
+	<h2>Ajout d'un client</h2>
+	<div align="center">
+	<table>
+    <tr>
+    <?php
+      include "connect.php";
+      $vConnect = fConnect();
+      $vQuery = "SELECT numero_client, nom , prenom , email FROM proClients ORDER BY nom, prenom";
+      $vResult=pg_query($vConnect, $vQuery);
+      echo "<TR>";
+      echo "<TD>Client:</TD><TD><select name='numero_cl'>";
+      while ($row = pg_fetch_array($vResult)){
+        echo "<option value='$row[0]'> $row[1] $row[2] $row[3]</option>";
+      }
+      echo "</select></TD>";
+      echo "</TR>";
+
+      $vQuery = "SELECT id, denomination, stock FROM proMarchandise WHERE stock>0 ORDER BY denomination";
+      $vResult=pg_query($vConnect, $vQuery);
+      echo "<TR>";
+      echo "<TD>Produit :</TD><TD><select name='id_produit'>";
+      while ($row = pg_fetch_array($vResult)){
+        echo "<option value='$row[0]'> $row[1] Stock: $row[2]</option>";
+      }
+      echo "</select></TD>";
+      echo "</TR>";
+      pg_close($vConnect);
+    ?>
+
+
+    <tr>
+      <td>Date de livraison :</td><td><select name="jour" id="jour">  <option value="">Jour</option>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+					<option value="6">6</option>
+					<option value="7">7</option>
+					<option value="8">8</option>
+					<option value="9">9</option>
+					<option value="10">10</option>
+					<option value="11">11</option>  
+					<option value="12">12</option>
+					<option value="13">13</option>  
+					<option value="14">14</option>
+					<option value="15">15</option>  
+					<option value="16">16</option>
+					<option value="17">17</option>  
+					<option value="18">18</option>
+					<option value="19">19</option>
+					<option value="20">20</option>  
+					<option value="21">21</option>
+					<option value="22">22</option>  
+					<option value="23">23</option>
+					<option value="24">24</option>
+					<option value="25">25</option> 
+					<option value="26">26</option>
+					<option value="27">27</option>
+					<option value="28">28</option>
+					<option value="29">29</option> 
+					<option value="30">30</option>   
+					<option value="31">31</option>  
+			</select>
+
+			<select name="mois" id="mois" >  <option value="" >Mois</option>
+					<option value="1">Janvier</option>
+					<option value="2">Février</option>
+					<option value="3">Mars</option>
+					<option value="4">Avril</option>
+					<option value="5">Mai</option>
+					<option value="6">Juin</option>
+					<option value="7" >Juillet</option>
+					<option value="8">Août</option>
+					<option value="9">Septembre</option>
+					<option value="10">Octobre</option>  
+					<option value="11">Novembre</option> 
+					<option value="12">Décembre</option>
+			</select>
+
+			<select  name="annee" id="annee">  <option value="">Ann&eacute;e</option>
+				<option value="2019">2019</option>
+				<option value="2018">2018</option>
+				<option value="2017">2017</option>
+				<option value="2016">2016</option><br>
+    </tr>
+    
+    
+	</table>
+
+	<br>
+	<br>
+	<br>
+		<input type="submit" value="Valider">
+		<input type="reset" value="Annuler">
+	</div>
+	</form>
+</body>
+
+</html>
