5c3f8b3f5acd684cab2185ebc990e73b81da6b19
MaximeBi <maxime.bigonne@gmail.com>
2016-05-10 19:21:16 +0200

    Update modification of client

--- src/ModifClient.php
+++ src/ModifClient.php
@@ -3,52 +3,52 @@ session_start();
 
 if(isset($_POST['go']) && $_POST['go']=="Modifier")
 {
-  $nom = $_POST['nom'];
-  $prenom= $_POST["prenom"];
-  $email= $_POST["email"];
-  $telephone= $_POST["telephone"];
+  $numRue = $_POST['num_rue'];
+  $route = $_POST['nomrue'];
+  $batiment = $_POST['batiment'];
+  $etage = $_POST['etage'];
+  $digicode = $_POST['digicode'];
 
-  if($telephone == ""){$telephone = 0;}//Erreur si je veux insérer un numéro de téléphone vierge
-
-  $id = $_SESSION['id'];
+  $id_user = $_SESSION['id'];
+  $id_adresse = $_SESSION['id_adresse'];
 
   $pattern = '/[][(){}<>\/+²"*%&=?`"\'^\!$_:;,]/';
 
     $AllAnswersOk = true;
 
     /*On vérifie que le nom et le prénom ne contiennent pas de caractères spéciaux*/
-    if (preg_match($pattern, $nom, $matches)){
+    if (preg_match($pattern, $route, $matches)){
         echo "Le nom contient des caractères spéciaux <br>";
         echo "<input type='button' value='Retour' onClick='history.go(-1)'>";
         $AllAnswersOk = false;
     }
 
-    else if (preg_match('/[0-9]/', $nom, $matches)){
+    else if (preg_match('/[0-9]/', $route, $matches)){
         echo "Le nom contient des chiffres <br>";
         echo "<input type='button' value='Retour' onClick='history.go(-1)'>";
         $AllAnswersOk = false;
     }
 
-    else if (preg_match($pattern, $prenom, $matches)){
+    else if (preg_match($pattern, $batiment, $matches)){
         echo "Le prénom contient des caractères spéciaux <br>";
         echo "<input type='button' value='Retour' onClick='history.go(-1)'>";
         $AllAnswersOk = false;
     }
 
-    else if (preg_match('/[0-9]/', $prenom, $matches)){
+    else if (preg_match('/[0-9]/', $batiment, $matches)){
         echo "Le prénom contient des chiffres <br>";
         echo "<input type='button' value='Retour' onClick='history.go(-1)'>";
         $AllAnswersOk = false;
     }
 
     /*On vérifie que le nom et le prénom ne sont pas seulement un ou des espaces*/
-    else if ((trim($nom, ' ')) == ''){
+    else if ((trim($route, ' ')) == ''){
        echo "Vous n'avez rentré que des espaces pour le nom <br>";
        echo "<input type='button' value='Retour' onClick='history.go(-1)'>";
        $AllAnswersOk = false;
     }
 
-    else if ((trim($prenom, ' ')) == ''){
+    else if ((trim($batiment, ' ')) == ''){
         echo "Vous n'avez rentré que des espaces pour le prénom <br>";
         echo "<input type='button' value='Retour' onClick='history.go(-1)'>";
         $AllAnswersOk = false;
@@ -58,8 +58,8 @@ if(isset($_POST['go']) && $_POST['go']=="Modifier")
       include "connect.php";
 
       $vConnect = fConnect();
-      $query = "UPDATE proClients SET prenom='$prenom', nom='$nom', telephone='$telephone', email='$email' WHERE numero_client='$id'";
-
+      $query = "UPDATE proAdresse a set numero_rue='$numRue', route_nom='$route', batiment='$batiment', etage='$etage', digicode='$digicode' WHERE id='$id_adresse'";
+      echo $query;
       $result =pg_query($vConnect, $query);
 
       if($result != FALSE){
--- src/modifClientForm.php
+++ src/modifClientForm.php
@@ -10,12 +10,13 @@ if(isset($_GET['id'])){
   include('connect.php');
   $vConnect = fConnect();
 
-  $vSql ="SELECT prenom, nom, telephone, email from proClients WHERE numero_client=".$id;
-  //$querySQL2 = "SELECT numero_de_rue, route_nom, batiment, etage, digicode from Client, Adresse WHERE numero_client = ".$id." and Client.route=Adresse.route_nom and Client.numero=Adresse.numero_de_rue ";
+
+  $vSql = "SELECT numero_rue, route_nom, batiment, etage, digicode, id from proClients, proAdresse WHERE numero_client = ".$id." and proClients.adresse=proAdresse.id";
 
   $result =pg_query($vConnect, $vSql);
 
-  $datas_client = pg_fetch_array($result);
+  $datas_client = pg_fetch_row($result);
+
 
 }
 else{
@@ -39,16 +40,38 @@ else{
     <div align="center">
       <table>
         <tr>
-          <td>Nom :</td><td><input type="text" name="nom" maxlength="25" value=<?php echo $datas_client[1]; ?> required> *</td><br>
+          <?php $c_row = current($datas_client) ; next($datas_client); next($datas_client);?>
+          <td>Numéro de rue :</td><td><input type="text" name="num_rue" maxlength="25" value=<?php echo $c_row; ?> required> *</td><br>
+        </tr>
+        <?php
+          $vQuery = "SELECT * FROM proRoute ORDER BY route_nom";
+          $vResult=pg_query($vConnect, $vQuery);
+          echo "<TR>";
+          echo "<TD>Adresse:</TD><TD><select name='nomrue'>";
+          while ($row = pg_fetch_array($vResult)){
+            echo "<option value='$row[0]'> $row[1] $row[0] </option>";
+          }
+          echo "</select></TD>";
+          echo "</TR>";
+          pg_close($vConnect);
+        ?>
+        <tr>
+          <?php $c_row = current($datas_client) ; next($datas_client); ?>
+          <td>Batiment :</td><td><input type="text" name="batiment" maxlength="50" value=<?php echo $c_row; ?>> </td><br>
         </tr>
         <tr>
-    			<td>Prénom :</td><td><input type="text" name="prenom" maxlength="25" value=<?php echo $datas_client[0]; ?> required> *</td><br>
-    		</tr>
-        <tr>
-          <td>E-mail :</td><td><input type="text" name="email" maxlength="50" value=<?php echo $datas_client[3]; ?> required> *</td><br>
+          <?php $c_row = current($datas_client) ; next($datas_client); ?>
+          <td>Etage :</td><td><input type="text" name="etage" length="10" value=<?php echo $c_row; ?> > </td><br>
         </tr>
         <tr>
-          <td>Téléphone :</td><td><input type="text" name="telephone" length="10" value=<?php echo $datas_client[2]; ?>></td><br>
+          <?php $c_row = current($datas_client) ; next($datas_client); ?>
+          <td>Digicode :</td><td><input type="text" name="digicode" length="10" value=<?php echo $c_row; ?>> </td><br>
+
+          <?php
+            $c_row = current($datas_client) ;
+            $_SESSION['id_adresse'] = $c_row;
+          ?>
+          
         </tr>
       </table>
 
@@ -58,33 +81,6 @@ else{
         <input name="go" type="submit" value="Modifier">
     </div>
   </form>
-
-  <!--form method="POST" action="ModifClient.php">
-    <div align="center">
-      <table>
-        <tr>
-          <td>Numéro de rue :</td><td><input type="text" name="num_rue" maxlength="25" value=<?php echo $datas_client[1]; ?> required> *</td><br>
-        </tr>
-        <tr>
-          <td>Route :</td><td><input type="text" name="route" maxlength="25" value=<?php echo $datas_client[0]; ?> required> *</td><br>
-        </tr>
-        <tr>
-          <td>Batiment :</td><td><input type="text" name="batiment" maxlength="50" value=<?php echo $datas_client[2]; ?> required> *</td><br>
-        </tr>
-        <tr>
-          <td>Etage :</td><td><input type="text" name="etage" length="10" value=<?php echo $datas_client[3];?> required> *</td><br>
-        </tr>
-        <tr>
-          <td>Digicode :</td><td><input type="text" name="digicode" length="10" value=<?php echo $datas_client[4];?> required> *</td><br>
-        </tr>
-      </table>
-
-      <br>
-      <br>
-      <br>
-        <input name="go" type="submit" value="Modifier">
-    </div>
-  </form-->
 
 </body>
 </html
