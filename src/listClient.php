33f575fb6c9f07c04b532aacdb1913a9cc3f35af
MaximeBi <maxime.bigonne@gmail.com>
2016-05-10 19:30:05 +0200

    add <tr> below the listclient title

--- src/listClient.php
+++ src/listClient.php
@@ -15,7 +15,7 @@
     </head>
    <body>
       <h1>Liste des clients</h1>
-
+      <hr>
       <table align="center" border="1">
         <tr>
           <td><strong>ID</strong></td><td><strong>PRENOM</strong></td><td><strong>NOM</strong></td><td><strong>NUMERO</strong></td><td><strong>MAIL</strong></td>
