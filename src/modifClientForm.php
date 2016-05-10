34ddf50aee97ba0646aa896025fe53c6426bf782
MaximeBi <maxime.bigonne@gmail.com>
2016-05-10 10:56:11 +0200

    Add button to orders and new version about adress modification IMPORTANT: FILE NOT TEST

--- src/modifClientForm.php
+++ src/modifClientForm.php
@@ -9,7 +9,10 @@ if(isset($_GET['id'])){
 
   include('connect.php');
   $vConnect = fConnect();
+
   $vSql ="SELECT prenom, nom, telephone, email from proClients WHERE numero_client=".$id;
+  //$querySQL2 = "SELECT numero_de_rue, route_nom, batiment, etage, digicode from Client, Adresse WHERE numero_client = ".$id." and Client.route=Adresse.route_nom and Client.numero=Adresse.numero_de_rue ";
+
   $result =pg_query($vConnect, $vSql);
 
   $datas_client = pg_fetch_array($result);
@@ -27,6 +30,11 @@ else{
 
 <body>
   <h1>Modification client numéro <?php echo $id; ?></h1>
+  <hr>
+  <br>
+  <a href="#" target="_blank"><input type="button" value="COMMANDES CLIENTS"></a>
+  <br>
+
   <form method="POST" action="ModifClient.php">
     <div align="center">
       <table>
@@ -50,6 +58,33 @@ else{
         <input name="go" type="submit" value="Modifier">
     </div>
   </form>
+
+  <!--form method="POST" action="ModifClient.php">
+    <div align="center">
+      <table>
+        <tr>
+          <td>Numéro de rue :</td><td><input type="text" name="num_rue" maxlength="25" value=<?php echo $datas_client[1]; ?> required> *</td><br>
+        </tr>
+        <tr>
+          <td>Route :</td><td><input type="text" name="route" maxlength="25" value=<?php echo $datas_client[0]; ?> required> *</td><br>
+        </tr>
+        <tr>
+          <td>Batiment :</td><td><input type="text" name="batiment" maxlength="50" value=<?php echo $datas_client[2]; ?> required> *</td><br>
+        </tr>
+        <tr>
+          <td>Etage :</td><td><input type="text" name="etage" length="10" value=<?php echo $datas_client[3];?> required> *</td><br>
+        </tr>
+        <tr>
+          <td>Digicode :</td><td><input type="text" name="digicode" length="10" value=<?php echo $datas_client[4];?> required> *</td><br>
+        </tr>
+      </table>
+
+      <br>
+      <br>
+      <br>
+        <input name="go" type="submit" value="Modifier">
+    </div>
+  </form-->
 
 </body>
 </html
