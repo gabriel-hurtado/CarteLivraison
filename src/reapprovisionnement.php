dbf273bdbfd2c64d330d81ccf121d5a3824fa2b6
Valentin Montupet <valentin.mntp@gmail.com>
2016-05-31 14:16:25 +0200

    hotfix reapprovisionnement

--- src/reapprovisionnement.php
+++ src/reapprovisionnement.php
@@ -29,9 +29,70 @@
           echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td> <td> <input type='radio' name='produit' value='$row[0]''> </td></tr>";
         }
         ?>
+
     </table><br>
     <div align=center>
     Quantité reçue : <input type="numeric" name="qte"><br><br>
+    Nouveau réapprovisionnement :<select name="jour" id="jour">
+          <option value="">Jour</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="14">14</option>
+          <option value="15">15</option>
+          <option value="16">16</option>
+          <option value="17">17</option>
+          <option value="18">18</option>
+          <option value="19">19</option>
+          <option value="20">20</option>
+          <option value="21">21</option>
+          <option value="22">22</option>
+          <option value="23">23</option>
+          <option value="24">24</option>
+          <option value="25">25</option>
+          <option value="26">26</option>
+          <option value="27">27</option>
+          <option value="28">28</option>
+          <option value="29">29</option>
+          <option value="30">30</option>
+          <option value="31">31</option>
+      </select>
+
+      <select name="mois" id="mois" >
+          <option value="" >Mois</option>
+          <option value="1">Janvier</option>
+          <option value="2">Février</option>
+          <option value="3">Mars</option>
+          <option value="4">Avril</option>
+          <option value="5">Mai</option>
+          <option value="6">Juin</option>
+          <option value="7" >Juillet</option>
+          <option value="8">Août</option>
+          <option value="9">Septembre</option>
+          <option value="10">Octobre</option>
+          <option value="11">Novembre</option>
+          <option value="12">Décembre</option>
+      </select>
+
+      <select  name="annee" id="annee">
+        <option value="">Ann&eacute;e</option>
+        <option value="2019">2019</option>
+        <option value="2018">2018</option>
+        <option value="2017">2017</option>
+        <option value="2016">2016</option>
+      </select>
+      <br><br>
+
     <input type="submit">
     </div>
   </form>
--- src/reapprovisionnementForm.php
+++ src/reapprovisionnementForm.php
@@ -12,24 +12,56 @@
 			$qte = $_POST['qte'];
 			$produit= $_POST["produit"];
 
+      $jour= $_POST["jour"];
+    	$mois= $_POST["mois"];
+    	$annee= $_POST["annee"];
+
 	    echo "<h2>Réaprovisionnement</h2>";
 	    echo "<div align=center>";
 
 			include "connect.php";//Fichier de connection à la BD
 
-		  $vConnect = fConnect(); //Connexion initiale ?
-
-      $vSql = "UPDATE proMarchandise SET stock = stock + '$qte' WHERE id= '$produit' ";
-      $result =pg_query($vConnect, $vSql);
-
-      if($result != FALSE){
-        echo "<h1>Réapprovisionnement effectué !</h1><br>";
-        echo "<a href=\"reapprovisionnement.php\"><input type='button' value='Retour'>";
-      }
-      else{
-        echo "<strong>ERROR UPDATE DATABASE</strong>";
+      if (empty($jour) or empty($mois) or empty($annee)) {
+      		$jour = '01';
+      		$mois = '01';
+      		$annee = '1970';
       }
 
+      	$date=$annee."-".$mois."-".$jour;
+
+      	if($date != '1970-01-01'){
+      		/* Date du jour */
+      		date_default_timezone_set('Europe/Paris');
+      		$today = date("Ymd");
+      		// on formate les dates selon le format Ymd
+      		$today = new DateTime( $today );
+      		$today = $today->format("Ymd");
+      		$date = new DateTime( $date );
+      		$date = $date->format("Ymd");
+      		/* On vérifie que la date de la séance est valide*/
+      		if (!checkdate($mois, $jour, $annee)){
+      				echo "Veuillez rentrer une date valide <br>";
+      				echo "<input type='button' value='Retour' onClick='history.go(-1)'>";
+      			}
+      		else if( $today > $date ) {
+      			echo "Le délai de réapprovisionnement ne peut etre dans le passé...<br>";
+      		  	echo "<input type='button' value='Retour' onClick='history.go(-1)'>";
+      		}
+        }
+      if ($date == '1970-01-01' || $today < $date){
+        $vConnect = fConnect(); //Connexion initiale ?
+
+        $vSql = "UPDATE proMarchandise SET stock = stock + '$qte', delai_reapprovisionnement='$date' WHERE id= '$produit' ";
+        $result =pg_query($vConnect, $vSql);
+
+        if($result != FALSE){
+          echo "<h1>Réapprovisionnement effectué !</h1><br>";
+          echo "<a href=\"reapprovisionnement.php\"><input type='button' value='Retour'>";
+        }
+        else{
+          echo "<strong>ERROR UPDATE DATABASE</strong>";
+        }
+      }
       echo "</div>";
 ?>
 
